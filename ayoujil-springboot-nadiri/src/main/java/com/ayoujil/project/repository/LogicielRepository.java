878c2685278b41b04816bc2c24c66005a6ac8da9
ayoujil3334 <106082052+ayoujil3334@users.noreply.github.com>
2022-05-29 22:18:50 +0100

    Delete ayoujil-springboot-nadiri directory

--- ayoujil-springboot-nadiri/src/main/java/com/ayoujil/project/repository/LogicielRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ayoujil.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ayoujil.project.model.Logiciel;
-
-public interface LogicielRepository extends JpaRepository <Logiciel, Long> {
-
-}
